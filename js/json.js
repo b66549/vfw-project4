9ef8279cce9d685dc56b137e856fa9418871b795
Rolando Velasco <rvelasco@fullsail.edu>
2013-03-26 23:12:07 -0700

    New json file with object data for testing main code.

---
+++ js/json.js
@@ -0,0 +1,31 @@
+// Name: Rolando Velasco
+// Term: VFW 1303
+// Project Part 4
+// JSON object
+
+var json = {
+	"goal1": {
+		"goalHeadline": ["Goal:", "Wash the car"],
+		"goalType": ["Goal Type:", "Personal"],
+		"deadline": ["Deadline:", "2014-10-10"],
+		"size": ["Size:", "7"],
+		"details": ["Details", "Get the car washed"],
+		"goalcomplete": ["Goal Complete:", "Goal Not Yet Achieved"]
+	}, 
+	"goal2": {
+		"goalHeadline": ["Goal:", "Hawaii trip"],
+		"goalType": ["Goal Type:", "Travel"],
+		"deadline": ["Deadline:", "2013-06-10"],
+		"size": ["Size:", "5"],
+		"details": ["Details", "Go on vacation to Hawaii this year"],
+		"goalcomplete": ["Goal Complete:", "Goal Not Yet Achieved"]
+	},
+	"goal3": {
+		"goalHeadline": ["Goal:", "Finish hw"],
+		"goalType": ["Goal Type:", "Education"],
+		"deadline": ["Deadline:", "2013-03-26"],
+		"size": ["Size:", "9"],
+		"details": ["Details", "Finish this week's hw"],
+		"goalcomplete": ["Goal Complete:", "Goal Achieved"]
+	}
+}
